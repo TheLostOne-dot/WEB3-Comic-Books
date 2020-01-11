3d0af3c921484790bb1387b0a194b6e7f700c2f7
Mishkov,Angel A.Y <a.mishkov@student.fontys.nl>
2020-01-11 02:54:46 +0100

    Image upload Controller and resize

--- web3/app/Http/Controllers/UserProfileController.php
+++ web3/app/Http/Controllers/UserProfileController.php
@@ -59,33 +59,27 @@ class UserProfileController extends Controller
             return back();
         }
     }
-
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         if($request->has('avatar')){
 
             $avatar = $request->file('avatar');
             $extension = $request->file('avatar')->getClientOriginalExtension();
 
-            $filename = md5(time()).'_'.$avatar->getClientOriginalName();
-
-            $normal = Image::make($avatar)->resize(160, 160)->encode($extension);
-            $medium = Image::make($avatar)->resize(80, 80)->encode($extension);
-            $small = Image::make($avatar)->resize(40, 40)->encode($extension);
+            $filename='avatar';
+            $ui=auth()->id();
+            $normal = Image::make($avatar)->resize(250 , 250)->encode($extension);
 
 
-            //'avatars/'.auth()->id(),"avatar",'s3'
-            //dd($normal);
+            Storage::disk('s3')->put('/avatars/'."{$ui}".'/'."{$filename}", (string)$normal, 'public');
 
-            Storage::disk('s3')->put('avatars/'.Auth::user()->id.'/'.$filename,(string)$small,'public');
-
-            //Storage::disk('s3')->put('/users/'.Auth::user()->uuid.'/avatar/medium/'.$filename, (string)$medium, 'public');
-
-            //Storage::disk('s3')->put('/users/'.Auth::user()->uuid.'/avatar/small/'.$filename, (string)$small, 'public');
-
+            
             $user = User::findorFail(Auth::user()->id);
             $user->avatar = $filename;
-            $user->save();
+//            $user->save();
 
             return redirect()->back();
+        }
     }
-}}
+
+}
